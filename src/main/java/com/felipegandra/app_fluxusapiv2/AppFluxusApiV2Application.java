c91abfb8a4b0e4ec2aa9a4d4dca45d5ce88a957c
fgandraf <fgandraf@gmail.com>
2024-09-08 20:37:28 -0300

    feat: add new project

---
+++ src/main/java/com/felipegandra/app_fluxusapiv2/AppFluxusApiV2Application.java
@@ -0,0 +1,13 @@
+package com.felipegandra.app_fluxusapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFluxusApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFluxusApiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/felipegandra/app_fluxusapiv2/AppFluxusApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipegandra.app_fluxusapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFluxusApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
